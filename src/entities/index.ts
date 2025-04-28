b417dd8a4e6ecd0a0e5b411e73d01b931fe4704e
Никита Домрачев <domrachev-nikitka@mail.ru>
2025-04-28 19:21:03 +0700

    /features:был сбилжен проект swiper был перемещен в widgets swiper-slider задан миксином с возможностью менять его так же было сделано с category-item вместо всех items там только один который задан миксином и который можно менять взависимости от необходимости

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,3 +1,2 @@
 import './example/index'
 import './music-player-song/music-player-song'
-import './swiper-slider/swiper-slider'
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,3 +1,4 @@
 import './index/index'
 import './header/headerController'
 import '../widgets/index/modules/music-player/music-player'
+import './swiper/swiper'
